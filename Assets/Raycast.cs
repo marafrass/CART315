e496bc56b391c9cf3462a7a3d65ed1c11ed1f7c0
marafrass <55090657+marafrass@users.noreply.github.com>
2021-02-03 11:39:35 -0500

    Added Raycast script and other in-class exercises

---
+++ Assets/Raycast.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+
+    public Camera maincamera;
+    public GameObject prefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        { 
+        
+        
+
+            Debug.Log("Clicked");
+
+            Ray clickray = maincamera.ViewportPointToRay(Input.mousePosition);
+            RaycastHit result;
+            if (Physics.Raycast(clickray, out result))
+            {
+                GameObject.Instantiate(prefab, result.point, Quaternion.identity);
+
+            }
+               
+        
+        }
+    }
+}
